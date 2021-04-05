8f48f0564aae358b4aa27d5043bb92fd64841b01
unknown <ben.norman34@gmail.com>
2021-04-04 18:15:29 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,22 @@ function setup() {
 function draw() {
 
   if (mouseIsPressed) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
+  stroke(map(mouseX, 0, 600, 0, 255, true))
+  polygon(width - mouseX, height - mouseY, 20, 6);
+  polygon(mouseX, mouseY, 20, 6);
+  polygon(width - mouseX, height - mouseY, 15, 6);
+  polygon(mouseX, mouseY, 15, 6);
   }
 
+}
+
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
