b39a211c9a2e92968379f36297d4efe6a00a5174
unknown <ben.norman34@gmail.com>
2021-04-11 16:16:55 -0700

    Updates to files

--- sketch.js
+++ sketch.js
@@ -46,7 +46,7 @@ function keyTyped() {
     saveCanvas(`fileName`, `png`);
   } else if (key == `d`) {
     //display Image
-
+    
     beginShape();
     for (let i = 0; i < array.length - 1; i++) {
       //polygon(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
