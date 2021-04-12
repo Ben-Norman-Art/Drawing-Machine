f2985810585a01bb7042817c545939d2f4e6400c
unknown <ben.norman34@gmail.com>
2021-04-11 19:55:04 -0700

    update files

--- sketch.js
+++ sketch.js
@@ -10,17 +10,17 @@ function setup() {
 function draw() {
 
   if (mouseIsPressed) {
-  //stroke(0, 10)
-  //let c = color(map(mouseX, 0, 900, 0, 255, true))
-  //noFill()
-  //square1(width - mouseX, height - mouseY, 20, 6);
-  //square1(mouseX, mouseY, 20, 6);
-  //square1(width - mouseX, height - mouseY, 15, 6);
-  //square1(mouseX, mouseY, 15, 6);
-  //square1(height - mouseX, width - mouseY, 20, 6);
-  //square1(width + mouseX, height + mouseY, 20, 6);
-  //square1(height - mouseX, width - mouseY, 15, 6);
-  //square1(height + mouseX, width + mouseY, 15, 6);
+  stroke(0, 10)
+  let c = color(map(mouseX, 0, 900, 0, 255, true))
+  noFill()
+  square1(width - mouseX, height - mouseY, 20, 6);
+  square1(mouseX, mouseY, 20, 6);
+  square1(width - mouseX, height - mouseY, 15, 6);
+  square1(mouseX, mouseY, 15, 6);
+  square1(height - mouseX, width - mouseY, 20, 6);
+  square1(width + mouseX, height + mouseY, 20, 6);
+  square1(height - mouseX, width - mouseY, 15, 6);
+  square1(height + mouseX, width + mouseY, 15, 6);
   array.push([mouseX, mouseY]);
 
 }
@@ -44,11 +44,7 @@ function keyTyped(){
 
   if (key == 's'){
   saveCanvas(`fileName`, `png`);
-  } else if (key === 'd'){
-
-  for(let i = 0; i < array.length - 1; i++){
-    line(array [i][0], array [i][1], array [i + 1][0], array [i + 1][1]);
-  }
+  } 
   if (key == 'c'){
   clear();
   background(220);
@@ -60,7 +56,5 @@ function keyTyped(){
 }
 
 function square1() {
-  stroke(0, 10)
-  noFill()
   rect(mouseX, mouseY, 10, 10);
 }
